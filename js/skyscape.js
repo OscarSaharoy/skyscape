b82107c1f28db5a36d9e889db218006f16c88017
Oscar Saharoy <osaharoy@gmail.com>
2022-12-13 19:38:19 +0000

    cleanups

--- js/skyscape.js
+++ js/skyscape.js
@@ -9,16 +9,9 @@ import skyFragmentShader from "../glsl/skyFragment.glsl.js";
 import { setupResize } from "./resize.js";
 
 export const canvas = document.querySelector( "#shader-canvas" );
-const renderer = new THREE.WebGLRenderer( {canvas: canvas, antialias: true} );
+const renderer      = new THREE.WebGLRenderer( {canvas: canvas, antialias: true} );
 
-const UP    = new THREE.Vector3(  0,  1,  0 );
-const DOWN  = new THREE.Vector3(  0, -1,  0 );
-const NORTH = new THREE.Vector3(  1,  0,  0 );
-const SOUTH = new THREE.Vector3( -1,  0,  0 );
-const EAST  = new THREE.Vector3(  0,  0,  1 );
-const WEST  = new THREE.Vector3(  0,  0, -1 );
-
-const scene = new THREE.Scene();
+const scene      = new THREE.Scene();
 scene.background = new THREE.Color( 0xb01050 );
 
 const aspect = canvas.width / canvas.height;
@@ -26,9 +19,6 @@ const fov    = Math.min( 60 * Math.max( 1, 1/aspect ), 100 );
 const near   = 0.1;
 const far    = 30;
 export const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-const cameraForward = new THREE.Vector3( 0, 0, -1 );
-camera.lookAt( cameraForward );
-//renderer.setPixelRatio(dpr);
 
 export const renderScene = () => renderer.render(scene, camera);
 
@@ -37,8 +27,8 @@ setupResize( canvas, camera, renderer );
 
 export const skyUniforms = {
     uTime:        { value: 0. },
-    uResolution:  { value: new THREE.Vector2() },
 	uZoom:        { value: 1. },
+    uResolution:  { value: new THREE.Vector2() },
 	uSkyRotation: { value: new THREE.Matrix4() },
 	uSunDir:      { value: new THREE.Vector3() },
 	uMoonDir:     { value: new THREE.Vector3() },
@@ -55,10 +45,9 @@ const skyMaterial = new THREE.ShaderMaterial({
 
 
 {
-    const geometry = 
-        new THREE.IcosahedronGeometry( 1, 3 );
+    const geometry = new THREE.IcosahedronGeometry( 1, 3 );
     const material = skyMaterial; 
-    const sphere = new THREE.Mesh( geometry, material );
+    const sphere   = new THREE.Mesh( geometry, material );
     scene.add( sphere );
 
     // render the sphere first and clear depth buffer 
