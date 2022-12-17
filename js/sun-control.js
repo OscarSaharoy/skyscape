ed6376449c5ff41329b8f2f9f17a2eb6bed905d1
Oscar Saharoy <osaharoy@gmail.com>
2022-12-17 22:35:10 +0000

    adjust pointer over sun by zoom

--- js/sun-control.js
+++ js/sun-control.js
@@ -6,14 +6,18 @@ import { canvas, skyUniforms } from "./skyscape.js";
 
 export function pointerOverSun( camera, event ) {
 
+	// get the canvas bounding box to find the pointer's location in clip space
     const canvasBox = canvas.getBoundingClientRect();
     const clipX =   event.clientX / canvasBox.width  * 2 - 1;
     const clipY = - event.clientY / canvasBox.height * 2 + 1;
 
+	// find the pointer direction in world space
     const clipVec = new THREE.Vector3( clipX, clipY, 0 );
 	const direction = clipVec.unproject( camera ).normalize();
 
-	return direction.dot( skyUniforms.uSunDir.value ) > 0.98;
+	// calculate whether the pointer is close enough to the sun adjusting for zoom
+	const paddingScaleFactor = camera.getEffectiveFOV() / camera.fov;
+	return direction.dot( skyUniforms.uSunDir.value ) > 0.99999 - paddingScaleFactor * 0.0005;
 }
 
 export function setSunDirection( camera, meanRelativePointer ) {
