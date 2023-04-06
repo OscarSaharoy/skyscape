1b0a8174660337e30f464c0a21bf6a18257896ff
Oscar Saharoy <osaharoy@gmail.com>
2023-04-06 14:44:43 +0100

    cleanup skyscape

--- js/skyscape.js
+++ js/skyscape.js
@@ -1,16 +1,14 @@
 // Oscar Saharoy 2022
 
-// divert errors to alerts so we can debug on mobile easier
-//window.onerror = error => alert(error);
-
 import * as THREE from './three.module.js'; 
 import vertexShader from "../glsl/vertex.glsl.js";
 import skyFragmentShader from "../glsl/skyFragment.glsl.js";
 import { uniforms } from "./uniforms.js";
 
 
-const scene      = new THREE.Scene();
-scene.background = new THREE.Color( 0xb01050 );
+const screenScene = new THREE.Scene();
+screenScene.background = new THREE.Color( 0xb01050 );
+
 
 const skyMaterial = new THREE.ShaderMaterial({
     vertexShader: vertexShader,
@@ -18,15 +16,14 @@ const skyMaterial = new THREE.ShaderMaterial({
     uniforms: uniforms,
     side: THREE.BackSide,
 });
-
-const geometry = new THREE.IcosahedronGeometry( 1, 3 );
-const sphere   = new THREE.Mesh( geometry, skyMaterial );
-scene.add( sphere );
+const skyGeometry = new THREE.IcosahedronGeometry( 1, 3 );
+const skySphere   = new THREE.Mesh( skyGeometry, skyMaterial );
+screenScene.add( skySphere );
 
 
 export function renderScreen( renderer, camera ) {
 
 	renderer.setRenderTarget( null );
-	renderer.render( scene, camera );
+	renderer.render( screenScene, camera );
 }
 
