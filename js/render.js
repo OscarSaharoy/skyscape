916d423217cef1896bdf2c26310ba9a405c2b4b3
Oscar Saharoy <osaharoy@gmail.com>
2023-09-30 14:32:00 +0100

    re add don't render when not mmoving

--- js/render.js
+++ js/render.js
@@ -18,6 +18,8 @@ import { renderAtmosphereLight } from "./atmosphere-light.js";
 ( function renderLoop() {
 	requestAnimationFrame( renderLoop );
 
+	if( uniforms.uFramesStationary.value++ >= 1 ) return;
+
 	renderAtmosphereLight( renderer, camera );
 	renderScreen( renderer, camera );
 } )();
